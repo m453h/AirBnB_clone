d3d2ac740588959fb9eec3130fec4e52beec67a3
Michael Hudson <mnkotagu@gmail.com>
2023-05-14 21:56:10 +0300

    Add more tests for FileStorage

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -69,5 +69,54 @@ class TestFileStorage(unittest.TestCase):
         self.assertTrue(hasattr(FileStorage, "_FileStorage__file_path"))
         self.assertTrue(hasattr(FileStorage, "_FileStorage__objects"))
 
+    def file_storage_save_action(self, classname):
+        """Helper method for save() method of the FileStorage class."""
+        cls = storage.classes().get(classname)
+        if cls is None:
+            raise ValueError(f"Invalid classname '{classname}'")
+        obj = cls()
+        storage.new(obj)
+        key = f"{type(obj).__name__}.{obj.id}"
+        storage.save()
+        file_path = FileStorage._FileStorage__file_path
+        self.assertTrue(os.path.isfile(file_path))
+        with open(file_path, "r", encoding="utf-8") as file:
+            file_contents = file.read()
+            self.assertIn(key, file_contents)
+            self.assertIn('"created_at":', file_contents)
+            self.assertIn('"updated_at":', file_contents)
+            expected_dict = {key: obj.to_dict()}
+            actual_dict = json.loads(file_contents)
+            self.assertEqual(actual_dict, expected_dict)
+
+    def test_save_base_model(self):
+        """Tests save() method for BaseModel class."""
+        self.file_storage_save_action("BaseModel")
+
+    def test_save_user_class(self):
+        """Tests save() method for User class."""
+        self.file_storage_save_action("User")
+
+    def test_save_state_class(self):
+        """Tests save() method for State class."""
+        self.file_storage_save_action("State")
+
+    def test_save_city_class(self):
+        """Tests save() method for City class."""
+        self.file_storage_save_action("City")
+
+    def test_save_amenity_class(self):
+        """Tests save() method for Amenity class."""
+        self.file_storage_save_action("Amenity")
+
+    def test_save_place_class(self):
+        """Tests save() method for Place class."""
+        self.file_storage_save_action("Place")
+
+    def test_save_review_class(self):
+        """Tests save() method for Review class."""
+        self.file_storage_save_action("Review")
+
+
 if __name__ == '__main__':
     unittest.main()
